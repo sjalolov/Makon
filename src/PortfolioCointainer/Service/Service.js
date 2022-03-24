683871fd0d814da43956e36e8233ace0fc8d269f
Shohrukh Jalolov <sjalolov@lclark.edu>
2022-03-25 01:54:50 +0600

    New Changes to Service Page

---
+++ src/PortfolioCointainer/Service/Service.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Service.css";
+
+export default function Service() {
+  return (
+    <body>
+      <h1>Our Service</h1>
+      <div class="service-section">
+        <div class="row"></div>
+        <div class="row">
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-hammer"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-brush"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-wrench"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-truck-pickup"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-broom"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="icon-wrapper">
+                <i class="fas fa-plug"></i>
+              </div>
+              <h3>Service Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quisquam consequatur necessitatibus eaque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
